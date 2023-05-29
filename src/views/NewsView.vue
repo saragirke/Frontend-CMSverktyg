aaab3074da772fcb9cc2b070ba2ca415f0d4d4d3
Your Name <sagi1700@student.miun.se>
2023-05-29 12:14:58 +0200

    pagn

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,107 +1,91 @@
 <template>
+  <div>
+    <h1 class="pt-7 text-center mb-4 text-3xl font-bold leading-none tracking-tight md:text-5xl lg:text-5xl dark:text-white">Nyheter</h1>
 
-  <h1 class="pt-7 text-center mb-4 text-3xl font-bold leading-none tracking-tight md:text-5xl lg:text-5xl dark:text-white"> Nyheter </h1>
+    <!-- v-for loop för att hämta nyhetsinlägg -->
+    <News v-for="news in paginatedNews" :news="news" :key="news.id" />
+    <br />
 
-   <!-- v-for loop för att hämta nyhetsinlägg -->
-   <News  v-for="news in sortedNews" :news="news" :key="news.id"/>
-   <br />
+    <!-- Paginering -->
+    <div class="flex justify-center">
+      <button @click="previousPage" :disabled="currentPage === 1">Föregående</button>
+      <button @click="nextPage" :disabled="currentPage === totalPages">Nästa</button>
+    </div>
+  </div>
 </template>
-
 
 <script>
 // Importering av komponenter
 import News from "../components/News.vue";
 
 export default {
- // Reaktiv data
- data() {
-   return {
-     news: [],
-   };
- },
- 
- computed: {
-   //Sorterar senaste nyheten längst upp på sidan
-   sortedNews: function() {
-                 return this.news.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated))
-             }
-   },
+  components: {
+    News,
+  },
 
- components: {
-   News,
- },
+  data() {
+    return {
+      news: [],
+      currentPage: 1,
+      newsPerPage: 5,
+    };
+  },
 
- methods: {
-/*********************************************
-* GET - Hämta nyheter
-*********************************************/
-   async getNews() {
-     //Fetch till restwebbtjänst
-     const resp = await fetch("https://cmsverktyg.azurewebsites.net/api/apinews", {
-       method: "GET",
-       headers: {
-         "Content-type": "application/json",
-       },
-     });
+  computed: {
+    sortedNews() {
+      return this.news.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+    },
+    totalPages() {
+      return Math.ceil(this.sortedNews.length / this.newsPerPage);
+    },
+    paginatedNews() {
+      const startIndex = (this.currentPage - 1) * this.newsPerPage;
+      const endIndex = startIndex + this.newsPerPage;
+      return this.sortedNews.slice(startIndex, endIndex);
+    },
+  },
 
-     // Data lagras
-     const data = await resp.json();
-     this.news = data;
-   },
-
-   async addComment() {
-      //Kontrollerar att fälten är ifyllda
-      if (
-       // (this.commentName != 0) &
-        (this.commentPost != 0)
-      ) {
-        //Javascript objekt
-        let CommentObj = {
-          //  commentName: this.commentName,
-            commentPost: this.commentPost,
-        };
-
-        //FETCH
-        const resp = await fetch("https://cmsverktyg.azurewebsites.net/api/apiComment", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify(CommentObj),
-        });
-
-        const data = await resp.json();
-
-
-        this.$router.go()
-        alert("Tack för din kommentar!" );
-
-      } 
-      else {
-       document.getElementById("error").innerHTML ="<p>" +"Alla fält behöver fyllas i!" + "</p>";
+  methods: {
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
       }
-    }, 
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    // ... övriga metoder ...
+  },
 
- },
- mounted() {
-   this.getNews();
+  async mounted() {
+    await this.getNews();
+  },
 
- },
+  async getNews() {
+    const resp = await fetch("https://cmsverktyg.azurewebsites.net/api/apinews", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await resp.json();
+    this.news = data;
+  },
 };
-
-
 </script>
 
 <style scoped>
-   p{
-    font-family:  'Poppins', sans-serif;
-    font-size: 16px;
-    color:black;
-    line-height: 1.7;
-   }
+p {
+  font-family: "Poppins", sans-serif;
+  font-size: 16px;
+  color: black;
+  line-height: 1.7;
+}
 
-   h1 {
-    font-family:  'Poppins', sans-serif;
-   }
-   </style>
+h1 {
+  font-family: "Poppins", sans-serif;
+}
+</style>
